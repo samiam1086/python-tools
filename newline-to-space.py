3cc20e93f10f175f758fce39b487ebeb09dcc5f7
Daniel <57763139+samiam1086@users.noreply.github.com>
2023-07-13 14:22:30 -0400

    Update newline-to-space.py

--- newline-to-space.py
+++ newline-to-space.py
@@ -3,7 +3,7 @@ import argparse
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('file', action='store', type=str, help='The file to parse')
-	parser.add_argument('-of', action='store', type=str, default='out.txt', help='File to output to')
+	parser.add_argument('-of', action='store', type=str, default='newline-to-spaceout.txt', help='File to output to. Default=./newline-to-spaceout.txt')
 	options = parser.parse_args()
 
 
