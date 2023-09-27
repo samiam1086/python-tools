040eac1aabfb8baed1f4cb4bb212a281523621a8
Daniel <57763139+samiam1086@users.noreply.github.com>
2023-09-27 16:12:55 -0400

    Update wwwchk.py

--- wwwchk.py
+++ wwwchk.py
@@ -4,17 +4,19 @@ import requests
 import argparse
 import sys
 
+
 def get_title(response_text):
     if response_text.find('<title') == -1:
         return ''
 
     tmp = response_text[response_text.find('<title'):]
-    tmp1 = ' Title: ' + tmp[tmp.find('>')+1:tmp.find('</title>')]
+    tmp1 = ' Title: ' + tmp[tmp.find('>') + 1:tmp.find('</title>')]
     return tmp1
+
 
 def https_chk(target):
     try:
-        x = requests.get('https://{}'.format(target), timeout=5, verify=False) # make an https request
+        x = requests.get('https://{}'.format(target), timeout=5, verify=False)  # make an https request
         if options.i is not None:
             if str(x.status_code) not in options.i:
                 print(x.url + " Status Code: " + str(x.status_code) + get_title(x.text))
@@ -28,9 +30,9 @@ def https_chk(target):
                 with open(options.o, 'a') as f:
                     f.write(x.url + " Status Code: " + str(x.status_code) + get_title(x.text) + '\n')
                     f.close()
-                    
+
     except KeyboardInterrupt:
-            sys.exit(1)
+        sys.exit(1)
     except BaseException as e:
         if options.se:
             if str(e).find('Max retries exceeded with url') != -1:
@@ -53,7 +55,6 @@ if __name__ == '__main__':
     parser.add_argument('-debug', action='store_true', help='Turn on debugging')
     parser.add_argument('-se', action='store_false', help='Skip any errors from printing')
 
-
     # Suppress only the single warning from urllib3 needed.
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
     if len(sys.argv) == 1:
@@ -62,25 +63,28 @@ if __name__ == '__main__':
 
     options = parser.parse_args()
 
-    with open(options.inputfile, 'r') as f: # read our inputfile
+    with open(options.inputfile, 'r') as f:  # read our inputfile
         dat = f.read()
         f.close()
 
-    target_list = dat.split('\n') # split it into a list
+    target_list = dat.split('\n')  # split it into a list
 
-    if options.i is not None: # if they gave us an options.i lets make it a list the split function does not cause issues if there is no comma it will just appear as 1 string in the list
+    if options.i is not None:  # if they gave us an options.i lets make it a list the split function does not cause issues if there is no comma it will just appear as 1 string in the list
         options.i = options.i.split(',')
 
     for target in target_list:
         try:
             if len(target) > 0:
-                x = requests.get('http://{}'.format(target), timeout=5) # make an http request
+                x = requests.get('http://{}'.format(target), timeout=5)  # make an http request
+                
+                if x.text.find('HTTP request was sent to HTTPS port') != -1:
+                    https_chk(target)
 
                 if options.i is not None:
-                    if str(x.status_code) not in options.i: # check if our response is a code designated to be ignroed
-                        print(x.url + " Status Code: " + str(x.status_code) + get_title(x.text)) # print our output
+                    if str(x.status_code) not in options.i:  # check if our response is a code designated to be ignroed
+                        print(x.url + " Status Code: " + str(x.status_code) + get_title(x.text))  # print our output
                         if options.o is not None:
-                            with open(options.o, 'a') as f: # if options.o then save the target
+                            with open(options.o, 'a') as f:  # if options.o then save the target
                                 f.write(x.url + " Status Code: " + str(x.status_code) + get_title(x.text) + '\n')
                                 f.close()
                 else:
@@ -91,7 +95,7 @@ if __name__ == '__main__':
                             f.close()
         except KeyboardInterrupt:
             sys.exit(1)
-        except requests.exceptions.ConnectionError: # if the server is running https we should get this
+        except requests.exceptions.ConnectionError:  # if the server is running https we should get this
             https_chk(target)
         except ConnectionResetError:
             https_chk(target)
