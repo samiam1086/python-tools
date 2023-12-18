81658147310e974e543b4faf385907e902270ed2
Daniel <57763139+samiam1086@users.noreply.github.com>
2023-12-18 13:48:41 -0500

    Update passwd2ntlm.py

--- passwd2ntlm.py
+++ passwd2ntlm.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 try:
     from passlib.hash import nthash
 except BaseException:
@@ -7,8 +7,20 @@ except BaseException:
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2 or '-h' in sys.argv or '--help' in sys.argv or '-help' in sys.argv:
+    if len(sys.argv) < 2 or '-h' in sys.argv or '--help' in sys.argv or '-help' in sys.argv: # input checking
         print('Usage: python3 passwd2ntlm.py \'password\'')
+        print('Usage: python3 passwd2ntlm.py password_file.txt')
+        print('NOTE: the password file must be 1 password per line')
         sys.exit(0)
-    print('password:nthash')
-    print('{}:{}'.format(sys.argv[1], nthash.hash(sys.argv[1])))
+
+    if os.path.isfile(sys.argv[1]): # check if password is a file
+        with open(sys.argv[1], 'r') as f: # read passwords from file into an array named dat should be ['passwd1\n', 'paddwd2\n', 'goodpassword\n']
+            dat = f.readlines()
+            f.close()
+
+        print('password:nthash')
+        for password in dat: # iterate through the dat array
+            print('{}:{}'.format(password[:len(password)], nthash.hash(password[:len(password)]))) # convert the 'passwd\n' to 'passwd' and make it an nt hash
+    else:
+        print('password:nthash')
+        print('{}:{}'.format(sys.argv[1], nthash.hash(sys.argv[1])))
