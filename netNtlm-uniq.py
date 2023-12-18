0984c9d998d3edc814eb9a17974e4489f23c47ca
Daniel <57763139+samiam1086@users.noreply.github.com>
2023-12-18 14:05:36 -0500

    Update netNtlm-uniq.py

--- netNtlm-uniq.py
+++ netNtlm-uniq.py
@@ -1,24 +1,29 @@
-import argparse
+import argparse, sys
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('file', action='store', type=str, help='The file to parse')
-	parser.add_argument('-of', action='store', type=str, default='netNtlm-uniqout.txt', help='File to output to. Default=./netNtlm-uniqout.txt')
-	options = parser.parse_args()
-	
-	data = []
+    parser = argparse.ArgumentParser(epilog='This can also be done using a one liner made by c@b0053\n"cat hashesfile | sort -u -t \':\' -k 1,2 | grep -v \'\$\'"')
+    parser.add_argument('file', action='store', type=str, help='The file to parse')
+    parser.add_argument('-of', action='store', type=str, default='netNtlm-uniqout.txt', help='File to output to. Default=./netNtlm-uniqout.txt')
 
-	with open(options.file, 'r') as f:
-		data = f.readlines()
-		f.close()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
 
-	uniq_data = []
-	uniq_data_full = []
-	for item in data:
-		if item.split("::")[0] not in uniq_data and item.split("::")[0].find('$') == -1:
-		        uniq_data_full.append(item)
-		        uniq_data.append(item.split("::")[0])
+    options = parser.parse_args()
 
-	with open(options.of, 'w') as f:
-		for thing in uniq_data_full:
-		        f.write(thing)
+    data = []
+
+    with open(options.file, 'r') as f:
+        data = f.readlines()
+        f.close()
+
+    uniq_data = []
+    uniq_data_full = []
+    for item in data:
+        if item.split("::")[0] not in uniq_data and item.split("::")[0].find('$') == -1:
+            uniq_data_full.append(item)
+            uniq_data.append(item.split("::")[0])
+
+    with open(options.of, 'w') as f:
+        for thing in uniq_data_full:
+            f.write(thing)
